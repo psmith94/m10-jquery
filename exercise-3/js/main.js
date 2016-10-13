da24e63d60d56dd3ad745d7a94a7ec98938e0b50
psmith94 <psmith9@uw.edu>
2016-10-13 10:17:47 -0700

    end of class

--- exercise-3/js/main.js
+++ exercise-3/js/main.js
@@ -3,9 +3,28 @@
 // You'll have to wait for you page to load to assign events to the elements created in your index.html File
 $(function() {
   // Select all <p> elements and assign a click event that removes the clicked element (recall `this`)
+  $('p').on('click', function() {
+  	$(this).remove();
+  });
   // Select the <circle>, and when it's clicked, turn it red
+  $('circle').on('click', function() {
+  	$(this).attr('style', 'fill:red');
+  });
   // Each time the <circle> is clicked, change its radius from 10 to 40
+  $('circle').on('click', function() {
+  	var rad = 10;	
+  	if ($(this).attr('r') == 10) {
+  		rad = 40;
+  	}
+  	$(this).attr('r', rad);
+  
+  });
   // Assign a mouseenter event to the <rect> element that makes it have an opacity of .5
+  $('rect').on('mouseenter', function() {
+  	$(this).css('opacity', .5);
+  });
   // Assign a mouseleave event to the <rect> element that makes it have an opacity of 1
-
+  $('rect').on('mouseleave', function() {
+  	$(this).css('opacity', 1);
+  });
 });
--- exercise-4/js/main.js
+++ exercise-4/js/main.js
@@ -3,10 +3,20 @@
 // You'll have to wait for you page to load to assign events to the elements created in your index.html File
 $(function() {
   // Animate your div with id my-div to these new set of properties:
+      
+	$('#my-div').animate({
+  		height: '300px',
+  		width: '500px',
+  		opacity: '.5'
+  	});
+
+
+
       // height of 300
       // width of 500
       // opacity of .5
 
   // When the transition finishes, fade out your paragraphs and your circle.
-
+  $('#fadetxt').fadeOut(500);
+  $('circle').fadeOut(500);
 });
